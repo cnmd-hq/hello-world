892ccd7973a1056cdf34b89aae76145168faa067
unknown <1732137236@qq.com>
2021-04-07 16:44:34 +0800

    hello

---
+++ hello/src/main/java/com/example/hello/HelloApplication.java
@@ -0,0 +1,23 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resources;
+
+@SpringBootApplication
+@RestController
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name",defaultValue = "World")String name){
+        return String.format("Hello world",name);
+    }
+
+}
